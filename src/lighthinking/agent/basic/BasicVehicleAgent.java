b81b084bc743c3ae82a0532a00d8f562d9f1ba96
aslmbc13 <asl_mbc_13@hotmail.com>
2016-12-07 01:53:37 +0000

    Fix

--- src/lighthinking/agent/basic/BasicVehicleAgent.java
+++ src/lighthinking/agent/basic/BasicVehicleAgent.java
@@ -5,19 +5,8 @@ import lighthinking.agent.VehicleAgent;
 
 @SuppressWarnings("serial")
 public class BasicVehicleAgent extends VehicleAgent {
-
 	public BasicVehicleAgent(String s, AgentManager mngr) {
 		super(s, mngr);
 		this.type = Type.BASIC;
-	}
-
-	@Override
-	public void update() {
-		super.update();
-	}
-	
-	@Override
-	public void finish() {
-		super.finish();
 	}
 }
